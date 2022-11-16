437d33f8d40589ee1d3c6fedda350d96f9d2d216
AjMcSaucy <IjahoneA@gmail.com>
2022-11-16 04:04:14 -0500

    Add playerSelection to RPS
    Function playerSelection was adding allowing the user
    to make a decision between rock, paper, or scissors.
    Returning it to the function.
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function getComputerChoice() {
-    cpuChoices = ['Rock', 'Paper', 'Scissors'];
+    cpuChoices = ['rock', 'paper', 'scissors'];
     cpuMath = Math.floor(Math.random() * 3);
     ans = cpuChoices[cpuMath];
     return ans;
 }
+console.log(getComputerChoice())
+
+function playerSelection() {
+    plyChoice = prompt('Choice your fighter: rock, paper, or scissors!');
+    ans = plyChoice.toLowerCase();
+    let i = 0;
+    while (ans != 'rock' && ans != 'paper' && ans != 'scissors') {
+        plyChoice = prompt('pick a damn option: rock, paper, or scissors!!!');
+        i++;
+        if (i > 2) {
+            return ("please try again and refresh the page");
+        }
+    };
+    return ans;
+}
+console.log(playerSelection())
+
+
+function RPS() {
+    switch (game) {
+    }
+}
+
