d44389d3bc271870050bc2f16a4134d7992bab7e
AjMcSaucy <IjahoneA@gmail.com>
2022-12-03 21:27:00 -0500

    Add playRound function and beginning of game func.
    Addition of playRound function which compares the players
    input with the computers random decision, responding with
    a win or loss.
    
    Intoduction of the game function which will allow the RPS
    to be ran up to five times.
    
    P.S. Commit earlier next time.
    
    [Ticket: X]

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A program where a person goes against a computer in 5 matches of rock paper scis
 I plan to use the random() functions * 3 to decide what answer to choose from an array, and return the choice.
 - playerSelection() function for obtaining users answer :
 I plan to prompt the user for an answer, take the string ignoring case-sensitivity, and return the given answer.
-- game() function used to run the choices against each other :
+- RPS() function used to run the choices against each other :
 I plan to compare selections made using if and if else statements or using switch statements.
 I plan to use a 'for' loop to keep track of how many times the game is played.
 I plan to use an incrementor variable to keep count of the scores.
--- script.js
+++ script.js
@@ -1,29 +1,86 @@
 function getComputerChoice() {
     cpuChoices = ['rock', 'paper', 'scissors'];
     cpuMath = Math.floor(Math.random() * 3);
-    ans = cpuChoices[cpuMath];
-    return ans;
+    ans1 = cpuChoices[cpuMath];
+    return ans1;
 }
-console.log(getComputerChoice())
+
+const compAns = getComputerChoice();
 
 function playerSelection() {
     plyChoice = prompt('Choice your fighter: rock, paper, or scissors!');
-    ans = plyChoice.toLowerCase();
+    ans2 = plyChoice.toLowerCase();
     let i = 0;
-    while (ans != 'rock' && ans != 'paper' && ans != 'scissors') {
+    while (ans2 != 'rock' && ans2 != 'paper' && ans2 != 'scissors') {
         plyChoice = prompt('pick a damn option: rock, paper, or scissors!!!');
         i++;
         if (i > 2) {
             return ("please try again and refresh the page");
+        } else if ((ans2 = "rock") || (ans2 = 'paper') || (ans2 = 'scissors')) {
+            break;
         }
     };
-    return ans;
+    return ans2;
 }
-console.log(playerSelection())
 
+const plyAns = playerSelection();
 
-function RPS() {
-    switch (game) {
+function playRound(plyAns, compAns) {
+    if (compAns === plyAns) {
+        return "Tie Game";
+    } else if ((plyAns === "rock") && (compAns === "scissors")) {
+        return "Win";
+    } else if ((plyAns === "rock") && (compAns === "paper")) {
+        return "Loss";
+    } else if ((plyAns === 'paper') && (compAns === 'rock')) {
+        return "Win";
+    } else if ((plyAns === 'paper') && (compAns === 'scissors') ) {
+        return "Loss";
+    } else if ((plyAns === 'scissors') && (compAns === 'paper') ) {
+        return "Win";
+    } else if ((plyAns === 'scissors') && (compAns === 'rock') ) {
+        return 'Loss';
+    } else {
+        return "how did you screw this up ?";
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let d = 0;
+        let Loss = 0;
+        playRound();
+        if (playRound() = 'Win') {
+            d++;
+        }
+    }
+}
+
+console.log(compAns);
+console.log(plyAns);
+console.log(playRound(plyAns, compAns));
+
+
+
+/*
+function RPS() {
+    if (compAns === plyAns) {
+        return "You both chose the same thing ! Tie game !";
+    } else if ((plyAns = "rock") && (compAns = "scissors")) {
+        return "You Win ! Rock smashes the scissors !";
+    } else if ((plyAns = "rock") && (compAns = "paper")) {
+        return "You Lose ! Rock got eaten by Paper ! Wa Wa Wa";
+
+    } else if ((plyAns = "paper") && (compAns = "rock")) {
+        return "You Win ! Paper eats Rock !";
+    } else if ((plyAns = "paper") && (compAns = "scissors")) {
+        return "You Lose ! Paper got diced scissors ! Wa Wa Wa"
+    } else if ((plyAns = "scissors") && (compAns = "paper")) {
+        return "You Win ! Scissors diced the paper !"
+    } else if ((plyAns = "scissors") && (compAns = "rock")) {
+        return "You Lose ! Scissors gets smashed by rock ! Wa Wa Wa"
+    } else {
+        return "How did you screw this up "
+    }
+}
+console.log(RPS()) */
